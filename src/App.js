662a85df09e0887c580175f1b586af0c1ae4cae3
Prince <princeinsan1919@gmail.com>
2020-10-12 15:58:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+// import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
+  const [images, setimages] = useState([]);
+  const [input, setinput] = useState("dogs");
+  // const useStyles = makeStyles((theme) => ({
+  //   root: {
+  //     flexGrow: 1,
+  //   },
+  //   paper: {
+  //     padding: theme.spacing(2),
+  //     textAlign: "center",
+  //     color: theme.palette.text.secondary,
+  //   },
+  // }));
+  // const classes = useStyles();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://pixabay.com/api/?key=18671218-994ed45afa93d10fd5be08e94&per_page=5&q=${input}`
+      )
+      .then((e) => setimages(e.data.hits))
+      .catch((e) => console.log(e));
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Images</h1>
+      <input value={input} onChange={(e) => setinput(e.target.value)} />
+      <button className="btn btn-primary">Search</button>
+
+      <div className="row">
+          {images.map((image) => (
+            <div className="col-md-3">
+              <img
+                className="img-fluid"
+                key={image.id}
+                alt={image.tags}
+                src={image.webformatURL}
+              />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
 export default App;
