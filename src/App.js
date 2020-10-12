94ff012eb77bceef9806fac2b8cf5ce3d78ebac7
Prince <princeinsan1919@gmail.com>
2020-10-12 16:28:13 +0530

    Added Max Count

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 function App() {
   const [images, setimages] = useState([]);
   const [input, setinput] = useState("dogs");
+  const [count, setcount] = useState(10);
   // const useStyles = makeStyles((theme) => ({
   //   root: {
   //     flexGrow: 1,
@@ -21,29 +22,44 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        `https://pixabay.com/api/?key=18671218-994ed45afa93d10fd5be08e94&per_page=5&q=${input}`
+        `https://pixabay.com/api/?key=18671218-994ed45afa93d10fd5be08e94&per_page=${count}&q=${input}`
       )
       .then((e) => setimages(e.data.hits))
       .catch((e) => console.log(e));
-  }, [input]);
+  }, [input, count]);
 
   return (
-    <div className="App container">
+    <div className="App container p-4">
       <h1>Images</h1>
-      <input value={input} onChange={(e) => setinput(e.target.value)} />
-      <button className="btn btn-primary">Search</button>
-
+      <div className="form-row">
+        <input
+          className="form-control col-md-10"
+          placeholder="Search your images"
+          value={input}
+          onChange={(e) => setinput(e.target.value)}
+        />
+        {/* &nbsp; <button className="btn btn-primary btn-sm">Search</button> */}
+        <input
+          className="form-control col-md-2"
+          placeholder="Max Results"
+          value={count}
+          onChange={(e) => setcount(e.target.value)}
+        />
+      </div>
+      <hr />
       <div className="row">
-          {images.map((image) => (
-            <div className="col-md-3">
+        {images.map((image) => (
+          <div className="col-md-3 custom-div">
+            <div className="card">
               <img
-                className="img-fluid"
+                className="img-fluid custom-img"
                 key={image.id}
                 alt={image.tags}
                 src={image.webformatURL}
               />
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </div>
   );
